86d4a2de14e41a10611b3d60eb4a2e227e272b21
baldm0mma <jev.forsberg@gmail.com>
2019-08-14 15:31:59 -0600

    Ready Heroku deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/burgers', (req, res) => res.send('It\'s burger time!!!!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log('App is running!'));
