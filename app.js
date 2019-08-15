e6a2d081356fd8ae36ea9a8aff57c88c402a0556
baldm0mma <jev.forsberg@gmail.com>
2019-08-15 16:56:55 -0600

    Add all gets

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
 const express = require('express');
+const dbConnection = require('./connection');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/api/v1/burgers', (req, res) => res.send("It's burger time!!!!"));
+app.get('/api/v1/burgers', (req, res) => {
+  dbConnection('burgers')
+    .select('*')
+    .then(burgers => res.status(200).json({ burgers }))
+    .catch(error => res.json('Could not retrieve all burgers'));
+});
+
+app.get('/api/v1/burgers/allIngredients', (req, res) => {
+  dbConnection('ingredients')
+    .select('*')
+    .then(ingredients => res.status(200).json({ ingredients }))
+    .catch(error => res.json('Could not retrieve all ingredients'));
+});
+
+app.get('/api/v1/burgers/:id', (req, res) => {
+  const { id } = req.params;
+  dbConnection('burgers')
+    .select('*')
+    .where({ id })
+    .then(burger => res.status(200).json(burger[0]))
+    .catch(error => res.json('Could not retrieve burger'));
+});
+
+app.get('/api/v1/burgers/:id/recipe', (req, res) => {
+  const { id } = req.params;
+  let recipe = {
+    burger: null,
+    ingredients: []
+  }
+  let codes = []
+  dbConnection('burgers')
+    .select('*')
+    .where({ id })
+    .then(burger => recipe.burger = burger[0])
+    .then(dbConnection('burger_ingredients')
+      .where('burger_id', id)
+      .then(b => b.map(el => recipe.ingredients.push(el.ingredients_id)))
+      .then(parsed => res.status(200).json(recipe)))
+      .catch(error => res.json('Could not retrieve recipe'));
+});
 
 app.listen(PORT, () => console.log('App is running!'));
+
+
+// app.get('/api/v1/burgers/:id/recipe', (req, res) => {
+//   const { id } = req.params;
+//   let recipe = {
+//     burger: null,
+//     ingredients: []
+//   }
+//   dbConnection('burgers')
+//     .select('*')
+//     .where({ id })
+//     .then(burger => recipe.burger = burger[0])
+//     .then(dbConnection('burger_ingredients')
+//       .where('burger_id', id)
+//       .then(jointIds => jointIds.map(id => dbConnection('ingredients')
+//         .where('id', id.ingredients_id)
+//         .then(burgerIngredients => burgerIngredients.map(bI => recipe.ingredients.push(bI)))
+//         .then(parsed => res.status(200).json(recipe)))
+//         .catch(error => res.json('Could not retrieve recipe'))));
+// });
